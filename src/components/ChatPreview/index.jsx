8146df380fcc453c64b3edf9daff957143547a4f
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-09-08 23:38:22 +0200

    feat: styles progresses

--- src/components/ChatPreview/index.jsx
+++ src/components/ChatPreview/index.jsx
@@ -2,10 +2,17 @@ import './style.css'
 
 const ChatPreview = ({chat}) => {
     return (
-        <div >
-            <span> {'< icon >'} </span>
-            <span> {' < Name >'} </span>
-            <span> {' < partial last msg >'} </span>
+        <div id={'chatPreview'}>
+            <div className={'d-flex flex-row align-items-center'}>
+                <img className={'m-2'}
+                     src="https://via.placeholder.com/60"
+                     alt="chatImg"/>
+                <div className={'d-flex flex-column align-items-left justify-items-center'}>
+                    <span> {' < Name >'} </span>
+                    <span className={'partial-msg'}> {' < partial last msg >'} </span>
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -6,18 +6,32 @@ const ProfilePreview = () =>
     <div id={'loggedUserPreview'} className={'d-flex align-items-center p-2 '}>
         <img className={'m-1'}
              src="https://via.placeholder.com/60"
-            alt="profilePic"/>
-        <h3 className={'m-1'} >
+             alt="profilePic"/>
+        <h3 className={'m-1'}>
             {'Name'} {'Surname'}</h3>
     </div>
-const SearchUser = () => <div>
 
-</div>
 
-const CreateGroup = () => <div><span> {'< CreateGroup >'} </span></div>
+const SearchUser = () => {
 
-const Sidebar = () => {
-    const chats = [{}, {}, {}, {}, {}, {}, ]
+    return (
+        <form id={'searchUser'} className="form-inline d-flex flex-row">
+            <input className="form-control mr-sm-2" type="search" placeholder="Find a user" aria-label="Search"/>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    )
+}
+
+
+const CreateGroup = () =>
+    <form id={'groupCreation'} className="form-inline d-flex flex-row">
+        <input className="form-control mr-sm-2" type="search" placeholder="Chat Name" aria-label="Search"/>
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Chat</button>
+    </form>
+
+const Sidebar = () =>
+{
+    const chats = [{}, {}, {}, {}, {}, {},]
     return (
         <div id={'leftSidebar'}
              className={'d-flex flex-column justify-content-between'}>
