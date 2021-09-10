97d31bfed665b20547a83e638e7a8035b5b3879d
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-09-10 12:32:53 +0200

    iter: messages component decoupled

---
+++ src/components/Messages/MessageEditor/index.jsx
@@ -0,0 +1,47 @@
+import "quill/dist/quill.snow.css";
+import "./style.css"
+import {useQuill} from "react-quilljs";
+import {useEffect, useState} from "react";
+import {Button, Col, Row} from "react-bootstrap";
+
+const MessageEditor = ({ sendMessageFunction  }) => {
+    const {quill, quillRef} = useQuill();
+    const [message, setMessage] = useState("");
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        sendMessageFunction( message )
+        setMessage("");
+    };
+
+    const updateMessageContent = () => {
+        if (quill) {
+            quill.on('text-change', () => {
+                const htmlMessage = quill.root.innerHTML
+                setMessage(htmlMessage)
+            });
+        }
+    }
+
+    useEffect(() => {
+        updateMessageContent()
+    }, [quill]);
+
+    return (
+        <form id={'messageEditor'}
+            onSubmit={onSubmitHandler}>
+            <Row>
+                <Col md={10}>
+                    <div className={'editor-wrapper '}>
+                        <div ref={quillRef}/>
+                    </div>
+                </Col>
+                <Col md={2} className={'d-flex flex-column align-items-center justify-content-center p-3'}>
+                    <Button type='submit'>send</Button>
+                </Col>
+            </Row>
+        </form>
+    )
+}
+
+export default MessageEditor
--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -5,72 +5,45 @@ import {useEffect, useState} from "react";
 import {connect} from "react-redux";
 import {io} from "socket.io-client";
 
-import "quill/dist/quill.snow.css";
+
 import {useQuill} from "react-quilljs";
 
 import Parser from 'html-react-parser';
 import {Button, Col, Row} from "react-bootstrap";
+import messageEditor from "./MessageEditor";
+import MessageEditor from "./MessageEditor";
+import MessageRows from "./MessageRows";
 
 
-const MessageRow = ({position, content = ''}) => {
-    position = position === "start" || position === "end" ? position : "";
-    return (
-        <div
-            className={`d-flex justify-content-${position}`}
-            style={{width: "100%", height: "max-content"}}>
-            <p className={`message ${position}`} style={{maxWidth: "75%", height: "max-content"}}>
-                {Parser(content)}
-            </p>
-        </div>
-    );
-};
+
 
 const Messages = (props) => {
-    // Specific Domain, message sending
-    const [message, setMessage] = useState("");
-
-    //out of domain, online users is expected in chats/rooms/groups previews
-    const [onlineUsers, setOnlineUsers] = useState([]);
-
-    //Domain of a High level component
+    const messages = [{}, {}, {}];
     const [loggedIn, setLoggedIn] = useState(false);
     const [chatHistory, setChatHistory] = useState([]);
 
-    // Specific Domain, message sending
-
-    const {quill, quillRef} = useQuill();
-
     const ADDRESS = process.env.REACT_APP_BACKEND;
     const socket = io(ADDRESS, {transports: ["websocket"]});
-    const checkOnlineUsers = async () => {
-        try {
-            const response = await fetch(ADDRESS + "/online-users");
-            const data = await response.json();
-            setOnlineUsers(data.onlineUsers);
-        } catch (error) {
-            console.log(error);
-        }
-    };
 
-
-    useEffect(() => {
-        console.log("myiD👓", props.user._id);
-        socket.on("connect", () => {
-            socket.emit("login", props.user.id);
-            console.log(socket.id, "🎨 socket");
+    const emitUserIsConnecting = (userID) => {
+        socket.on("connect", (  ) => {
+            socket.emit("login", userID );
         });
+    }
+
+    //More refactor here
+    useEffect(() => {
+        emitUserIsConnecting( props.user.id )
 
         socket.on("loggedIn", () => {
-            console.log("Successfully logged in!");
             setLoggedIn(true);
             checkOnlineUsers();
             socket.on("message", () => {
-                console.log("a new message was received!");
+
             });
         });
+        socket.on("newConnection", () =>{
 
-        socket.on("newConnection", () => {
-            console.log("new user logged in!");
         });
         return () => {
             socket.disconnect();
@@ -78,17 +51,9 @@ const Messages = (props) => {
     }, []);
 
 
-    const messages = [{}, {}, {}];
-    const setMessageFunc = (e) => {
-        console.log(e.target.value);
-        setMessage(e.target.value);
-        sendMessage(e);
-    };
 
-    const sendMessage = (e) => {
-        e.preventDefault();
+    const liveSendMessage = ( message ) => {
         if (loggedIn) {
-            console.log("sending new message...");
             const newMessage = {
                 text: message,
                 sender: props.user,
@@ -97,54 +62,19 @@ const Messages = (props) => {
             };
             socket.emit("sendmessage", newMessage);
             setChatHistory([...chatHistory, newMessage]);
-            setMessage("");
+
         }
-    };
-
-
-    useEffect(() => {
-        if (quill) {
-            quill.on('text-change', () => {
-
-                const htmlMessage = quill.root.innerHTML
-
-                setMessage(htmlMessage)
-            });
-        }
-    }, [quill]);
-
+    }
     return (
         <div id={'messages'} className={'d-flex flex-column align-items-end'}>
             <div style={{width: "100%", height: "100%"}}>
-                {messages
-                    .map((msg) => (
-                        <MessageRow
-                            key={Math.random()}
-                            position={msg.sender === props.user ? "end" : "start"}
-                        />
-                    ))
-                    .reverse()}
+                <MessageRows messages={ messages }/>
             </div>
-            {/*
-			<form onSubmit={(e) => setMessageFunc(e)}>
-				<input placeholder='enter message'/>
-				<button type='submit'>send</button>
-			</form>
-			*/}
-            <form onSubmit={sendMessage}>
-                <Row>
-                    <Col md={10}>
-                        <div className={'editor-wrapper '}>
-                            <div ref={quillRef}/>
-                        </div>
-                    </Col>
-                    <Col md={2} className={'d-flex flex-column align-items-center justify-content-center p-3'}>
-                        <Button type='submit'>send</Button>
-                    </Col>
-                </Row>
-            </form>
+            <MessageEditor sendMessageFunction={liveSendMessage}/>
         </div>
     );
 };
+
+
 
 export default connect((s) => s)(Messages);
